5362f822d5448a82adb24cb02e2bcef098987a1b
elijah <Elijah@Junjies-MacBook-Pro.local>
2014-09-02 23:37:03 +0800

    APUE

---
+++ figure_2_17.c
@@ -0,0 +1,34 @@
+#include "apue.h"
+#include <errno.h>
+#include <limits.h>
+
+#ifdef OPEN_MAX
+static long pathmax ＝ OPEN_MAX;
+#else
+static long pathmax = 0;
+#endif
+
+
+/* if PATH_MAX is indeterminate，no guarantee this is adequate */
+
+#define PATH_MAX_GUESS 256
+
+long open_max(void)
+{
+	if(openmax == 0)
+	{
+		errno = 0;
+		if((openmax = sysconf(_SC_OPEN_MAX)) < 0)
+		{
+			if(errno == 0)
+			{
+				openmax = OPEN_MAX_GUESS;
+			}
+			else
+			{
+				err_sys("sysconf error for _SC_OPEN_MAX");
+			}
+		}
+	}
+	return (openmax);
+}
